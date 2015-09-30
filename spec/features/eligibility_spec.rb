2a6d62e4c01f352535c97aa1b04361ea8c851001
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-09-30 16:50:18 -0500

    Finish landing page and elibility examples

--- spec/features/eligibility_spec.rb
+++ spec/features/eligibility_spec.rb
@@ -3,7 +3,7 @@
 describe 'A registered user', type: :feature do
   context 'in English' do
     it 'completes eligibility survey and is eligible' do
-      sign_in_pt_en(ENV['Pt_Email'], ENV['Pt_Password'])
+      sign_in_pt_en(ENV['Pt_101_Email'], ENV['Pt_101_Password'])
       visit "#{ENV['Base_URL']}/#/eligibility"
       find('.ng-binding', text: 'How old are you?')
       fill_in 'Q1', with: '25'
@@ -21,12 +21,151 @@ describe 'A registered user', type: :feature do
       expect(page).to have_content 'You are eligible to participate'
     end
 
-    it 'completes eligibility survey and is ineligible'
+    it 'completes eligibility survey and is ineligible due to age' do
+      sign_in_pt_en(ENV['Pt_102_Email'], ENV['Pt_102_Password'])
+      visit "#{ENV['Base_URL']}/#/eligibility"
+      find('.ng-binding', text: 'How old are you?')
+      fill_in 'Q1', with: '17'
+      qs = ['Are you currently a smoker?',
+            'Are you thinking of quitting smoking within the next 30 days?',
+            'Do you currently live in California?']
+      qs.each do |text|
+        within('.form-group', text: text) do
+          choose 'Yes'
+        end
+      end
+
+      check 'Ocean Park Health Center'
+      click_on 'Submit'
+      expect(page).to have_content 'You are not eligible to participate'
+    end
+
+    it 'completes eligibility survey, is ineligible due to neg response Q2' do
+      sign_in_pt_en(ENV['Pt_103_Email'], ENV['Pt_103_Password'])
+      visit "#{ENV['Base_URL']}/#/eligibility"
+      find('.ng-binding', text: 'How old are you?')
+      fill_in 'Q1', with: '17'
+      within('.form-group', text: 'Are you currently a smoker?') do
+        choose 'No'
+      end
+
+      within('.form-group', text: 'Are you thinking of quitting') do
+        choose 'Yes'
+      end
+
+      within('.form-group', text: 'Do you currently live in California?') do
+        choose 'Yes'
+      end
+
+      check 'Ocean Park Health Center'
+      click_on 'Submit'
+      expect(page).to have_content 'You are not eligible to participate'
+    end
+
+    it 'completes eligibility survey, is ineligible due to neg response Q3' do
+      sign_in_pt_en(ENV['Pt_104_Email'], ENV['Pt_104_Password'])
+      visit "#{ENV['Base_URL']}/#/eligibility"
+      find('.ng-binding', text: 'How old are you?')
+      fill_in 'Q1', with: '17'
+      within('.form-group', text: 'Are you currently a smoker?') do
+        choose 'Yes'
+      end
+
+      within('.form-group', text: 'Are you thinking of quitting') do
+        choose 'No'
+      end
+
+      within('.form-group', text: 'Do you currently live in California?') do
+        choose 'Yes'
+      end
+
+      check 'Ocean Park Health Center'
+      click_on 'Submit'
+      expect(page).to have_content 'You are not eligible to participate'
+    end
   end
 
   context 'in Español' do
-    it 'completes eligibility survey and is eligible'
+    it 'completes eligibility survey and is eligible' do
+      sign_in_pt_es(ENV['Pt_201_Email'], ENV['Pt_201_Password'])
+      visit "#{ENV['Base_URL']}/#/eligibility"
+      find('.ng-binding', text: '¿Cuántos años tiene?')
+      fill_in 'Q1', with: '25'
+      qs = ['¿Fuma usted actualmente?',
+            '¿Está pensando en dejar de fumar dentro de los próximos 30 días?',
+            '¿Actualmente vive en California?']
+      qs.each do |text|
+        within('.form-group', text: text) do
+          choose 'Sí'
+        end
+      end
 
-    it 'completes eligibility survey and is ineligible'
+      check 'Centro de Salud Ocean Park'
+      click_on 'Submit'
+      expect(page).to have_content 'You are eligible to participate'
+    end
+
+    it 'completes eligibility survey and is ineligible due to age' do
+      sign_in_pt_es(ENV['Pt_202_Email'], ENV['Pt_202_Password'])
+      visit "#{ENV['Base_URL']}/#/eligibility"
+      find('.ng-binding', text: '¿Cuántos años tiene?')
+      fill_in 'Q1', with: '17'
+      qs = ['¿Fuma usted actualmente?',
+            '¿Está pensando en dejar de fumar dentro de los próximos 30 días?',
+            '¿Actualmente vive en California?']
+      qs.each do |text|
+        within('.form-group', text: text) do
+          choose 'Sí'
+        end
+      end
+
+      check 'Centro de Salud Ocean Park'
+      click_on 'Submit'
+      expect(page).to have_content 'You are not eligible to participate'
+    end
+
+    it 'completes eligibility survey, is ineligible due to neg response Q2' do
+      sign_in_pt_es(ENV['Pt_203_Email'], ENV['Pt_203_Password'])
+      visit "#{ENV['Base_URL']}/#/eligibility"
+      find('.ng-binding', text: '¿Cuántos años tiene?')
+      fill_in 'Q1', with: '17'
+      within('.form-group', text: '¿Fuma usted actualmente?') do
+        choose 'No'
+      end
+
+      within('.form-group', text: '¿Está pensando en dejar de fumar') do
+        choose 'Sí'
+      end
+
+      within('.form-group', text: '¿Actualmente vive en California?') do
+        choose 'Sí'
+      end
+
+      check 'Centro de Salud Ocean Park'
+      click_on 'Submit'
+      expect(page).to have_content 'You are not eligible to participate'
+    end
+
+    it 'completes eligibility survey, is ineligible due to neg response Q3' do
+      sign_in_pt_es(ENV['Pt_204_Email'], ENV['Pt_204_Password'])
+      visit "#{ENV['Base_URL']}/#/eligibility"
+      find('.ng-binding', text: '¿Cuántos años tiene?')
+      fill_in 'Q1', with: '17'
+      within('.form-group', text: '¿Fuma usted actualmente?') do
+        choose 'Sí'
+      end
+
+      within('.form-group', text: '¿Está pensando en dejar de fumar') do
+        choose 'No'
+      end
+
+      within('.form-group', text: '¿Actualmente vive en California?') do
+        choose 'Sí'
+      end
+
+      check 'Centro de Salud Ocean Park'
+      click_on 'Submit'
+      expect(page).to have_content 'You are not eligible to participate'
+    end
   end
 end
--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -26,7 +26,8 @@ describe 'A visitor to the site', type: :feature do
     it 'confirms email' do
       visit "#{ENV['Base_URL']}/en/participants/confirmation?" \
             "confirmation_token=#{ENV['Pt_3_Confirmation']}"
-      click_on 'Sign in'
+      expect(page)
+        .to have_content 'Your email address has been successfully confirmed.'
       fill_in 'participant_email', with: ENV['Pt_3_Email']
       fill_in 'participant_password', with: ENV['Pt_3_Password']
       click_on 'Sign in'
@@ -35,8 +36,8 @@ describe 'A visitor to the site', type: :feature do
 
     it 'is a registered participant and signs in' do
       click_on 'Sign in'
-      fill_in 'participant_email', with: ENV['Pt_Email']
-      fill_in 'participant_password', with: ENV['Pt_Password']
+      fill_in 'participant_email', with: ENV['Pt_101_Email']
+      fill_in 'participant_password', with: ENV['Pt_101_Password']
       click_on 'Sign in'
       expect(page).to have_content 'Signed in successfully.'
     end
@@ -52,25 +53,90 @@ describe 'A visitor to the site', type: :feature do
       click_on 'Sign in'
       find('h2', text: 'Sign in')
       click_on 'Forgot your password?'
-      fill_in 'participant_email', with: ENV['Pt_Email']
+      fill_in 'participant_email', with: ENV['Pt_101_Email']
       click_on 'Send me reset password instructions'
       expect(page).to have_content 'You will receive an email with ' \
                                    'instructions on how to reset your ' \
                                    'password in a few minutes.'
     end
 
+    it "uses 'Didn't receive confirmation instructions?'" do
+      click_on 'Sign in'
+      find('h2', text: 'Sign in')
+      click_on "Didn't receive confirmation instructions?"
+      find('h2', text: 'Resend confirmation instructions')
+      fill_in 'participant_email', with: ENV['Pt_5_Email']
+      click_on 'Resend confirmation instructions'
+      expect(page).to have_content 'You will receive an email with ' \
+                                   'instructions for how to confirm your ' \
+                                   'email address in a few minutes.'
+    end
 
-    it "selects 'Didn't receive confirmation instructions?' from sign in page"
+    it 'is a registered participant who locks their account' do
+      click_on 'Sign in'
+      find('h2', text: 'Sign in')
+      18.times do
+        fill_in 'participant_email', with: ENV['Pt_105_Email']
+        fill_in 'participant_password', with: 'whoops'
+        click_on 'Sign in'
+        expect(page).to have_content 'Invalid email or password.'
+      end
 
-    it "selects 'Didn't receive unlock instructions?' from sign in page"
+      fill_in 'participant_email', with: ENV['Pt_105_Email']
+      fill_in 'participant_password', with: 'whoops'
+      click_on 'Sign in'
+      expect(page).to have_content 'You have one more attempt before your ' \
+                                   'account is locked.'
 
-    it "selects 'Sign in' from the sign up page"
+      fill_in 'participant_email', with: ENV['Pt_105_Email']
+      fill_in 'participant_password', with: 'whoops'
+      click_on 'Sign in'
+      expect(page).to have_content 'Your account is locked.'
+    end
 
-    it "select 'Forgot your password?' from the sign up page"
+    it "uses 'Didn't receive unlock instructions?'" do
+      click_on 'Sign in'
+      find('h2', text: 'Sign in')
+      click_on "Didn't receive unlock instructions?"
+      find('h2', text: 'Resend unlock instructions')
+      fill_in 'participant_email', with: ENV['Pt_6_Email']
+      click_on 'Resend unlock instructions'
+      expect(page).to have_content 'You will receive an email with ' \
+                                   'instructions for how to unlock your ' \
+                                   'account in a few minutes.'
+    end
 
-    it "selects 'Didn't receive confirmation instructions?' from sign up page"
+    it "selects 'Sign in' from the sign up page" do
+      click_on 'English'
+      click_on 'Sign up'
+      find('h2', text: 'Sign up')
+      click_on 'Sign in'
+      expect(page).to have_css('h2', text: 'Sign in')
+    end
 
-    it "selects 'Didn't receive unlock instructions?' from sign up page"
+    it "selects 'Forgot your password?' from the sign up page" do
+      click_on 'English'
+      click_on 'Sign up'
+      find('h2', text: 'Sign up')
+      click_on 'Forgot your password?'
+      expect(page).to have_css('h2', text: 'Forgot your password?')
+    end
+
+    it "selects 'Didn't receive confirmation instructions?' on sign up page" do
+      click_on 'English'
+      click_on 'Sign up'
+      find('h2', text: 'Sign up')
+      click_on "Didn't receive confirmation instructions?"
+      expect(page).to have_css('h2', text: 'Resend confirmation instructions')
+    end
+
+    it "selects 'Didn't receive unlock instructions?' from sign up page" do
+      click_on 'English'
+      click_on 'Sign up'
+      find('h2', text: 'Sign up')
+      click_on "Didn't receive unlock instructions?"
+      expect(page).to have_css('h2', text: 'Resend unlock instructions')
+    end
 
     it 'is a registered participant, has selected English previously, ' \
        ' English is set in subsequent logins'
@@ -95,9 +161,10 @@ describe 'A visitor to the site', type: :feature do
     end
 
     it 'confirms email' do
-      visit "#{ENV['Base_URL']}/en/participants/confirmation?" \
+      visit "#{ENV['Base_URL']}/es/participants/confirmation?" \
             "confirmation_token=#{ENV['Pt_4_Confirmation']}"
-      click_on 'Iniciar sésion'
+      expect(page)
+        .to have_content 'Tu cuenta ha sido confirmada satisfactoriamente.'
       fill_in 'participant_email', with: ENV['Pt_4_Email']
       fill_in 'participant_password', with: ENV['Pt_4_Password']
       click_on 'Iniciar sésion'
@@ -106,8 +173,8 @@ describe 'A visitor to the site', type: :feature do
 
     it 'is a registered participant and signs in in Español' do
       click_on 'Iniciar sésion'
-      fill_in 'participant_email', with: ENV['Es_Pt_Email']
-      fill_in 'participant_password', with: ENV['Es_Pt_Password']
+      fill_in 'participant_email', with: ENV['Pt_201_Email']
+      fill_in 'participant_password', with: ENV['Pt_201_Password']
       click_on 'Iniciar sésion'
       expect(page).to have_content 'Sesión iniciada.'
     end
@@ -123,24 +190,94 @@ describe 'A visitor to the site', type: :feature do
       click_on 'Iniciar sésion'
       find('h2', text: 'Iniciar sésion')
       click_on '¿Ha olvidado su contraseña?'
-      fill_in 'participant_email', with: ENV['Es_Pt_Email']
+      fill_in 'participant_email', with: ENV['Pt_201_Email']
       click_on 'Envíeme las instrucciones para resetear mi contraseña'
       expect(page).to have_content 'Recibirás un correo con instrucciones ' \
                                    'sobre cómo resetear tu contraseña en ' \
                                    'unos pocos minutos.'
     end
 
-    it "selects 'Didn't receive confirmation instructions?' from sign in page"
+    it "uses 'Didn't receive confirmation instructions?'" do
+      click_on 'Iniciar sésion'
+      find('h2', text: 'Iniciar sésion')
+      click_on '¿No ha recibido las instrucciones de confirmación?'
+      find('h2', text: 'Reenviar instrucciones de confirmación')
+      fill_in 'participant_email', with: ENV['Pt_8_Email']
+      click_on 'Reenviar instrucciones de confirmación'
+      expect(page).to have_content 'Vas a recibir un correo con ' \
+                                   'instrucciones sobre cómo confirmar tu ' \
+                                   'cuenta en unos minutos.'
+    end
 
-    it "selects 'Didn't receive unlock instructions?' from sign in page"
+    it 'is a registered participant who locks their account' do
+      click_on 'Iniciar sésion'
+      find('h2', text: 'Iniciar sésion')
+      18.times do
+        fill_in 'participant_email', with: ENV['Pt_205_Email']
+        fill_in 'participant_password', with: 'whoops'
+        click_on 'Iniciar sésion'
+        expect(page).to have_content 'Email o contraseña no válidos.'
+      end
 
-    it "selects 'Sign in' from the sign up page"
+      fill_in 'participant_email', with: ENV['Pt_205_Email']
+      fill_in 'participant_password', with: 'whoops'
+      click_on 'Iniciar sésion'
+      expect(page).to have_content 'Tienes un intento más antes de que tu ' \
+                                   'cuenta sea bloqueada.'
 
-    it "select 'Forgot your password?' from the sign up page"
+      fill_in 'participant_email', with: ENV['Pt_205_Email']
+      fill_in 'participant_password', with: 'whoops'
+      click_on 'Iniciar sésion'
+      expect(page).to have_content 'Tu cuenta está bloqueada.'
+    end
 
-    it "selects 'Didn't receive confirmation instructions?' from sign up page"
+    it "uses 'Didn't receive unlock instructions?'" do
+      click_on 'Iniciar sésion'
+      find('h2', text: 'Iniciar sésion')
+      click_on 'No ha recibido instrucciones para desbloquear?'
+      find('h2', text: 'Reenviar instrucciones para desbloquear')
+      fill_in 'participant_email', with: ENV['Pt_7_Email']
+      click_on 'Reenviar instrucciones para desbloquear'
+      expect(page).to have_content 'Vas a recibir instrucciones para ' \
+                                   'desbloquear tu cuenta en unos pocos ' \
+                                   'minutos.'
+    end
 
-    it "selects 'Didn't receive unlock instructions?' from sign up page"
+    it "selects 'Iniciar sésion' from the sign up page" do
+      click_on 'Español'
+      find('a', text: 'Registrarse').click
+      find('h2', text: 'Registrarse')
+      click_on 'Iniciar sésion'
+      expect(page).to have_css('h2', text: 'Iniciar sésion')
+    end
+
+    it "selects '¿Ha olvidado su contraseña?' from the sign up page" do
+      click_on 'Español'
+      find('a', text: 'Registrarse').click
+      find('h2', text: 'Registrarse')
+      click_on '¿Ha olvidado su contraseña?'
+      expect(page).to have_css('h2', text: '¿Ha olvidado su contraseña?')
+    end
+
+    it "selects '¿No ha recibido las instrucciones de confirmación?' on sign " \
+       'up page' do
+      click_on 'Español'
+      find('a', text: 'Registrarse').click
+      find('h2', text: 'Registrarse')
+      click_on '¿No ha recibido las instrucciones de confirmación?'
+      expect(page)
+        .to have_css('h2', text: 'Reenviar instrucciones de confirmación')
+    end
+
+    it "selects 'No ha recibido instrucciones para desbloquear?' from sign " \
+       'up page' do
+      click_on 'Español'
+      find('a', text: 'Registrarse').click
+      find('h2', text: 'Registrarse')
+      click_on 'No ha recibido instrucciones para desbloquear?'
+      expect(page)
+        .to have_css('h2', text: 'Reenviar instrucciones para desbloquear')
+    end
 
     it 'is a registered participant, has selected Español previously, ' \
        'Español is set on subsequent logins'
