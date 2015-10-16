ea735d6760087058d66da756fb29d8b4b6a638b9
Christopher Brenner <christopher.brenner1@northwestern.edu>
2015-10-16 12:03:55 -0500

    Add example for verifying eligible participant

--- spec/features/users/data_spec.rb
+++ spec/features/users/data_spec.rb
@@ -28,5 +28,24 @@ describe 'A user signs in', type: :feature, metadata: :user do
     expect(page).to have_content 'participant208@example.com'
   end
 
-  it 'sees a consented participant flagged accordingly'
+  it 'sees a consented participant flagged accordingly' do
+    within('.nav.nav-pills.nav-stacked') do
+      click_on 'Consent responses'
+    end
+
+    x = 0
+    until x == 5
+      find('.header', text: 'Responded at').click
+      yesterday = Date.today - 1
+      break if page.has_css?('.responded_at_field.datetime_type',
+                             text: "#{yesterday.strftime('%B %d')}")
+      x += 1
+    end
+
+    within first('.consent_response_row') do
+      find('a', text: 'Participant').click
+    end
+
+    expect(page).to have_content 'participant19@example.com'
+  end
 end
